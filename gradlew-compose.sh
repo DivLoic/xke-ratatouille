1eacc5b6451949322031f4adae48cdab833d9e0b
DivLoic <loicm.divad@gmail.com>
2020-07-25 11:14:01 +0200

    introduce ctop

--- gradlew-compose.sh
+++ gradlew-compose.sh
@@ -4,4 +4,10 @@ confluent local destroy > /dev/null 2>&1
 
 docker-compose down > /dev/null 2>&1
 
-./gradlew docker && docker-compose up -d && watch -n 30 -t --color 'docker-compose ps'
+./gradlew docker
+
+docker-compose up -d
+
+ctop
+
+docker-compose down
